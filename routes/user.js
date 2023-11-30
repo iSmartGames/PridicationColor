e21a66a00523a01a3219092c891ae7a484e927c5
Ram Prakash <sharma.ramprakash3@gmail.com>
2023-11-30 15:06:59 +0530

    second commit

--- routes/user.js
+++ routes/user.js
@@ -506,7 +506,7 @@ function generateOTP() {
         'cache-control': 'no-cache',
         'content-type': 'application/x-www-form-urlencoded'
       },
-      data: 'sender_id=BLKSMS&message=YOUR_MESSAGE&mobile_no=8058626456'
+      data: 'sender_id=JODSMS&message={#var#} is the OTP verify your mobile number for {#var#}. NEVER SHARE YOUR OTP WITH ANYONE. Jaipur SMS Hub Pvt Ltd&mobile_no=8058626456'
     })
       .then(response => {
         console.log(response.data);
